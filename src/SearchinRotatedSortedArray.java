4a2e8c5e792a5f9463808c823906112dc01917ed
QHS <quanhushan@gmail.com>
2018-11-21 00:51:41 +0900

    Search in Rotated Sorted Array

---
+++ src/SearchinRotatedSortedArray.java
@@ -0,0 +1,61 @@
+/*
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+
+You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+You may assume no duplicate exists in the array.
+
+Your algorithm's runtime complexity must be in the order of O(log n).
+
+Example 1:
+
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+ * */
+public class SearchinRotatedSortedArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// int[] input = { 4, 5, 6, 7, 0, 1, 2 };
+		int[] input = { 3, 5, 1 };
+		System.out.println(search(input, 3));
+	}
+
+	public static int search(int[] nums, int target) {
+
+		if (nums.length == 0) {
+			return -1;
+		}
+		int left = 0;
+		int right = nums.length - 1;
+
+		while (left <= right) {
+			int mid = (left + right) / 2;
+			if (nums[mid] == target) {
+				return mid;
+			} else if (nums[mid] < nums[right]) {
+				if (nums[mid] < target && target <= nums[right]) {
+					left = mid + 1;
+				} else {
+					right = mid - 1;
+				}
+			} else {
+				if (nums[left] <= target && target < nums[mid]) {
+					right = mid - 1;
+				} else {
+					left = mid + 1;
+				}
+			}
+
+		}
+
+		return -1;
+	}
+
+}
