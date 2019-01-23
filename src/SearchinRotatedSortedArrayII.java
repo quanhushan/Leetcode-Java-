889846cd44943e39ceb341a114fb8705e74845e5
QHS <quanhushan@gmail.com>
2019-01-23 20:29:01 +0900

    Search in Rotated Sorted Array II

---
+++ src/SearchinRotatedSortedArrayII.java
@@ -0,0 +1,61 @@
+/**
+ * Suppose an array sorted in ascending order is rotated at some pivot unknown
+ * to you beforehand.
+ * 
+ * (i.e., [0,0,1,2,2,5,6] might become [2,5,6,0,0,1,2]).
+ * 
+ * You are given a target value to search. If found in the array return true,
+ * otherwise return false.
+ * 
+ * Example 1:
+ * 
+ * Input: nums = [2,5,6,0,0,1,2], target = 0 Output: true Example 2:
+ * 
+ * Input: nums = [2,5,6,0,0,1,2], target = 3 Output: false Follow up:
+ * 
+ * This is a follow up problem to Search in Rotated Sorted Array, where nums may
+ * contain duplicates. Would this affect the run-time complexity? How and why?
+ */
+public class SearchinRotatedSortedArrayII {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] input = { 2, 5, 6, 0, 0, 1, 2 };
+		System.out.println(search(input, 3));
+	}
+
+	public static boolean search(int[] nums, int target) {
+
+		if (nums.length == 0) {
+			return false;
+		}
+		int left = 0;
+		int right = nums.length - 1;
+
+		while (left <= right) {
+			int mid = (left + right) / 2;
+			if (nums[mid] == target) {
+				return true;
+			} else if (nums[mid] < nums[right]) {
+				if (nums[mid] < target && target <= nums[right]) {
+					left = mid + 1;
+				} else {
+					right = mid - 1;
+				}
+			} else if (nums[mid] > nums[right]) {
+				if (nums[left] <= target && target < nums[mid]) {
+					right = mid - 1;
+				} else {
+					left = mid + 1;
+				}
+			} else {
+				right--;
+
+			}
+		}
+
+		return false;
+
+	}
+
+}
